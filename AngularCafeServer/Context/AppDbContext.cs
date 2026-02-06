2393a05eb72ac2af89099835a9759be773e0b3bd
MuhammedGazi <muhammed.celenk1@ogr.sakarya.edu.tr>
2026-02-06 21:48:07 +0300

    Add project files.

---
+++ AngularCafeServer/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using AngularCafeServer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularCafeServer.Context
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<About> Abouts { get; set; }
+    }
+}
